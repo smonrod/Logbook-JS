1bbd2917688f4a7bf964d342135b386da7905934
smonrod_work <smonrod.92@gmail.com>
2024-12-03 15:09:36 -0500

    Inicio V03

--- forEach.js
+++ forEach.js
@@ -1,5 +1,13 @@
 const pendientes = ['Tarea','Comer', 'Proyecto', 'Estudiar'];
 
+// Función Normal
+/*
 pendientes.forEach(function( pendiente, indice){
+    console.log(`${indice} - ${pendiente}`);
+});
+*/
+
+// Arrow Function
+pendientes.forEach( ( pendiente, indice) => {
     console.log(`${indice} - ${pendiente}`);
 });
